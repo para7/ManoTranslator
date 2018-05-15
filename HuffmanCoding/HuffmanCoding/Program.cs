cfc477f3f05097ff84feac701e04b8b6dcae39d2
para7 <minstrel.suppin@gmail.com>
2018-05-15 23:35:02 +0900

    数え上げて確率操作まで

--- HuffmanCoding/HuffmanCoding/Program.cs
+++ HuffmanCoding/HuffmanCoding/Program.cs
@@ -21,23 +21,56 @@ namespace HuffmanCoding
     {
         public char c;
         public double prob;
+
+        public ListData() { }
+
+        public ListData(char _c, double _prob)
+        {
+            c = _c;
+            prob = _prob;
+        }
     }
 
     static class Program
     {
+        static void InsertSort(LinkedList<ListData> list, ListData data)
+        {
+            //if(list.Count == 0)
+            //{
+            //    list.AddFirst(data);
+            //    return;
+            //}
+            
+            for (var l = list.First; l != null; l = l.Next)
+            {
+                if (data.prob < l.Value.prob)
+                {
+                    list.AddBefore(l, data);
+                    return;
+                }
+            }
+            list.AddLast(data);
+        }
+
         static void Main(string[] args)
         {
-            StreamReader reader
-                = new StreamReader(
-       "data.txt");
-            
+            StreamReader reader = new StreamReader("data.txt");
+
             string str = new string(reader.ReadToEnd().Where(a => a != '゛').ToArray());
 
             reader.Close();
 
+            //出現確率の操作
+            string manu = "まのちゃんかわい";
+
+            foreach(var c in manu)
+            {
+                str += new string(c, 4000);
+            }
+
             var dic = new SortedList<char, int>();
 
-            foreach(char c in str)
+            foreach (char c in str)
             {
                 if (dic.ContainsKey(c))
                 {
@@ -48,15 +81,30 @@ namespace HuffmanCoding
                     dic.Add(c, 1);
                 }
             }
-            
+
             var list = new LinkedList<ListData>();
 
-            foreach(var a in dic)
+            foreach (var d in dic)
             {
-                Console.WriteLine("{0} {1}",a.Key.ToString(), a.Value);
+                var hoge = new ListData();
+                hoge.prob = (double)d.Value / str.Length;
+                hoge.c = d.Key;
+                InsertSort(list, hoge);
             }
 
-            Console.WriteLine(dic.Count);
+            double p = 0;
+            foreach (var l in list)
+            {
+                if(manu.Contains(l.c))
+                {
+                    Console.Write("- ");
+                }
+
+                Console.WriteLine("{0} {1}", l.c, l.prob);
+                p += l.prob;
+            }
+
+            Console.WriteLine(p);
             Console.Write(str.Length);
         }
     }
