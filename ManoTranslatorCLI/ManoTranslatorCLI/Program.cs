d1fc6815899c43bac42554731e3e011637ee517e
para7 <minstrel.suppin@gmail.com>
2018-05-22 10:11:17 +0900

    ヘルプを追加

--- ManoTranslatorCLI/ManoTranslatorCLI/Program.cs
+++ ManoTranslatorCLI/ManoTranslatorCLI/Program.cs
@@ -67,13 +67,19 @@ namespace ManoTranslatorCLI
 
                 //オプションを判断
                 case 2:
-                    if(args[0] == "-d" || args[0] == "-decode")
+                    if(args[0] == "-d" || args[0] == "--decode")
                     {
                         print = Decode(args[1]);
+                    }
+                    else if(args[0] == "-e" || args[0] == "--encode")
+                    {
+                        print = Encode(args[1]);
                     }
                     else
                     {
                         //ヘルプを表示
+
+                        OutputHelp();
 
                         return;
                     }
@@ -81,6 +87,9 @@ namespace ManoTranslatorCLI
 
                 //ヘルプを表示
                 default:
+
+                    OutputHelp();
+
                     break;
             }
 
@@ -122,6 +131,38 @@ namespace ManoTranslatorCLI
             }
 
             return ret;
+        }
+
+        static void OutputHelp()
+        {
+            Console.WriteLine("引数として与えられた文字列を「ほわっ」と「むんっ」に変換します。");
+
+            Console.WriteLine("");
+            Console.WriteLine("引数１つの操作");
+            Console.WriteLine("");
+            Console.WriteLine("args[0] : 日本語の文字列");
+            Console.WriteLine("日本語を「ほわっ」と「むんっ」に変換します");
+            Console.WriteLine("");
+            Console.WriteLine("例：> ManoTranslator.exe まのちゃんかわいい");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("引数２つの操作");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("args[0] : -e | --encode");
+            Console.WriteLine("args[1] : 日本語の文字列");
+            Console.WriteLine("「ほわっ」と「むんっ」に変換します");
+            Console.WriteLine("");
+            Console.WriteLine("例：> ManoTranslator.exe -e まのちゃんかわいい");
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("args[0] : -d | --decode");
+            Console.WriteLine("args[1] : 「ほわっ」と「むんっ」に変換された文字列");
+            Console.WriteLine("日本語に戻します\n");
+            Console.WriteLine("");
+            Console.WriteLine("例：> ManoTranslator.exe -d ほわっほわっ...");
+            Console.WriteLine("");
         }
     }
 }
